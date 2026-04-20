bbee0afc86f6c37087d3fbb2abfb0d6e4700f9d1
Baiyrov <y_baiyrov@kbtu.kz>
2026-04-20 21:05:29 +0500

    подключил стили и исправил app.ts

--- gotel-frontend/src/app/app.ts
+++ gotel-frontend/src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
+  template: `
+    <app-navbar></app-navbar>
+    <router-outlet></router-outlet>
+    <div class="footer">
+      <div class="footer-logo">GOtel</div>
+      <p>© 2026 GOtel. Все права защищены.</p>
+    </div>
+  `
 })
 export class App {
-  protected readonly title = signal('gotel-frontend');
+  title = 'gotel-frontend';
 }
